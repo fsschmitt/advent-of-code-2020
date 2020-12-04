1a07c6b40043d6a151f6394546302703d20577ef
fsschmitt <492108+fsschmitt@users.noreply.github.com>
2020-12-04 19:26:51 +0000

    add day 3 challenge

---
+++ src/challenge/day3/main.ts
@@ -0,0 +1,26 @@
+const countTreeInSlope = (input: string[], xincrement: number, yincrement: number): number => {
+  let xpos = xincrement;
+  let ypos = yincrement;
+  let totalTrees = 0;
+  for (ypos; ypos < input.length; ypos+=yincrement) {
+    if (input[ypos][xpos] === '#') {
+      totalTrees++;
+    }
+    xpos += xincrement;
+    xpos = xpos % input[ypos].length;
+  }
+  return totalTrees;
+}
+
+export const a = (input: string[]): number => {
+  return countTreeInSlope(input, 3, 1);
+}
+
+export const b = (input: string[]): number => {
+  const res1 = countTreeInSlope(input, 1, 1);
+  const res2 = countTreeInSlope(input, 3, 1);
+  const res3 = countTreeInSlope(input, 5, 1);
+  const res4 = countTreeInSlope(input, 7, 1);
+  const res5 = countTreeInSlope(input, 1, 2);
+  return res1 * res2 * res3 * res4 * res5;
+}
